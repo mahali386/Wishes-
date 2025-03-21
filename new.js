4db265879aaaa8363a2d239ab2034315ed12db89
Coding_Hub <144131187+mahali386@users.noreply.github.com>
2025-03-21 06:49:48 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+
+
+
+  $(".title").click(function () {
+
+    $(".container").addClass("open");
+  });
+
+  $(".close").click(function () {
+    $(".container").removeClass("open");
+  });
+})
+
+;
